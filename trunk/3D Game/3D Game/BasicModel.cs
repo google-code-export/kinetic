3a60a77a5fdbabf1d856d116be3623bd7259d730
xk.izanagi@gmail.com <xk.izanagi@gmail.com@2b57c25c-fa9e-e9a8-dae6-12b03f823b56>
2011-04-25 01:16:26 +0000

    Initial commit. Camera is somewhat working, and a dummy tower is in place.

---
+++ trunk/3D Game/3D Game/BasicModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _3D_Game
+{
+    class BasicModel
+    {
+        public Model model { get; protected set; }      // to represent 3D model
+        protected Matrix world = Matrix.Identity;       // transform matrix
+
+        //// CONSTRUCTOR
+        public BasicModel(Model m)
+        {
+            model = m;
+        }
+
+        //// UPDATE
+        public virtual void Update()
+        {
+        }
+
+        //// DRAW
+        public void Draw(Camera camera)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect be in mesh.Effects)
+                {
+                    be.EnableDefaultLighting();
+                    be.Projection = camera.projection;
+                    be.View = camera.view;
+                    be.World = GetWorld() * mesh.ParentBone.Transform;
+                }
+
+                mesh.Draw();
+            }
+        }
+
+        //// GETWORLD
+        public virtual Matrix GetWorld()
+        {
+            return world;
+        }
+
+
+    }
+}
---
+++ trunk/3D Game/3D Game/RotatingModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _3D_Game
+{
+    class RotatingModel : BasicModel
+    {
+        Matrix rotation = Matrix.Identity;
+
+        public RotatingModel(Model m)
+            : base(m) { }
+
+        public override void Update()
+        {
+            rotation *= Matrix.CreateRotationY(MathHelper.Pi / 180);
+        }
+
+        public override Matrix GetWorld()
+        {
+            return world * rotation;
+        }
+
+    }
+}
